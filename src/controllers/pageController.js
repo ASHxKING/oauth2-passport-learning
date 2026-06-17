0f1d0511a18fb105db005b97d7ed2671d3d3dca2
ash <getseth00@gmail.com>
2026-06-18 00:09:45 +0530

    added custom secret component

--- src/controllers/pageController.js
+++ src/controllers/pageController.js
@@ -1,12 +1,37 @@
+import db from "../config/pg.js";
+
+
 const showHomePage = (req, res) => {
   res.render("home.ejs");
 };
 
-const showSecretsPage = (req, res) => {
-  res.render("secrets.ejs");
+const showSecretsPage = async (req,res)=>{
+   const result = await db.query(
+      "SELECT secret FROM users WHERE email=$1",
+      [req.user.email]
+   );
+
+   res.render("secrets.ejs",{
+      secret: result.rows[0].secret
+   });
+}
+
+const postSecret = async (req,res)=>{
+  try {
+    await db.query("UPDATE public.users SET secret= $1 WHERE email=$2",[req.body.secret,req.user.email])
+    res.redirect("/secrets")
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const showSubmitPage = (req, res) => {
+  res.render("submit.ejs");
 };
 
 export {
   showHomePage,
   showSecretsPage,
+  showSubmitPage,
+  postSecret
 };
--- src/routes/pageRoutes.js
+++ src/routes/pageRoutes.js
@@ -4,6 +4,8 @@ import { ensureAuthenticated } from "../middleware/authMiddleware.js";
 import {
   showHomePage,
   showSecretsPage,
+  showSubmitPage,
+  postSecret
 } from "../controllers/pageController.js";
 
 const router = express.Router();
@@ -15,5 +17,9 @@ router.get(
   ensureAuthenticated,
   showSecretsPage
 );
+
+router.get("/submit",ensureAuthenticated ,showSubmitPage)
+
+router.post("/submit",ensureAuthenticated ,postSecret)
 
 export default router;
